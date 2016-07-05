395a9de436663054430b72e88b763df328ba1c3f
Paul Hayes <fofr@users.noreply.github.com>
2016-07-05 17:39:10 +0100

    Add user to breadcrumb, show files on studies

--- app/routes.js
+++ app/routes.js
@@ -6,20 +6,24 @@ var User = require('./presenters/user');
 router.get('/', function (req, res) {
   promise = openCGAclient.users().info();
   promise.then(function(response) {
-    var user = new User(response);
+    var user = response.result[0];
     var projects = response.result[0].projects;
 
     res.render('index', { 'user' : user, 'projects': projects });
   });
 });
 
-router.get('/project/:id', function (req, res) {
-  promise = openCGAclient.projects().info(req.params.id);
-  promise.then(function(response) {
-    var project = response.result[0];
+router.get('/project/:projectId', function (req, res) {
+  projectPromise = openCGAclient.projects().info(req.params.projectId);
+  userPromise = openCGAclient.users().info();
+
+  Promise.all([projectPromise, userPromise]).then(function(responses) {
+    var project = responses[0].result[0],
+        user = responses[1].result[0];
+
     res.render('project', {
       'project' : project,
-      'studies' : project.studies
+      'user' : user
     });
   });
 });
@@ -27,13 +31,20 @@ router.get('/project/:id', function (req, res) {
 router.get('/project/:projectId/study/:studyId', function (req, res) {
   studyPromise = openCGAclient.studies().info(req.params.studyId);
   projectPromise = openCGAclient.projects().info(req.params.projectId);
-  Promise.all([studyPromise, projectPromise]).then(function(responses) {
-    var project = responses[1].result[0],
-        study = responses[0].result[0];
+  userPromise = openCGAclient.users().info();
+  filesPromise = openCGAclient.studies().files(req.params.studyId);
+
+  Promise.all([studyPromise, projectPromise, userPromise, filesPromise]).then(function(responses) {
+    var study = responses[0].result[0],
+        project = responses[1].result[0],
+        user = responses[2].result[0];
+        files = responses[3].result;
 
     res.render('study', {
       'project' : project,
-      'study' : study
+      'study' : study,
+      'user' : user,
+      'files': files
     });
   });
 });
